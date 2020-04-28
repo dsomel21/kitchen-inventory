e8f17d94761d82bc5488738e6ab2a186fc159578
Dilraj Grobo <dilraj@grobo.io>
2020-04-28 19:18:32 -0400

    Added the Manufacture and Address table with FKs

--- server/db/migrations/20200427200015_AddTablesWithFK.js
+++ server/db/migrations/20200427200015_AddTablesWithFK.js
@@ -4,39 +4,50 @@ const addDefaultColumns = (t) => {
   t.datetime('deleted_at');
 };
 
-const references = (table, foreignTableName) => {
-  table
-    .integer(`${foreignTableName}_id`)
-    .unsigned()
-    .references(`${foreignTableName}.id`)
-    .onDelete('CASCADE');
-};
-
 exports.up = async (knex) => {
   // Address
-  await knex.createTable('addresses', (t) => {
+  await knex.schema.createTable('addresses', (t) => {
     t.increments();
     t.string('street_address_1', 50).notNullable();
     t.string('street_address_2', 50);
     t.string('city', 50).notNullable();
     t.string('zipcode', 14).notNullable();
     t.specificType('coordinate', 'POINT');
-    references(t, 'states');
-    references(t, 'countries');
     // unsigned() => Only POSITIVE Integer
 
-    addDefaultColumns();
+    // BELONGS TO states
+    t.integer(`state_id`)
+      .unsigned()
+      .references(`states.id`)
+      .onDelete('CASCADE');
+
+    // BELONGS TO countries
+    t.integer(`country_id`)
+      .unsigned()
+      .references(`countries.id`)
+      .onDelete('CASCADE');
+
+    addDefaultColumns(t);
   });
 
   // Manufacturer
-  await knex.createTable('manufacturers', (t) => {
+  await knex.schema.createTable('manufacturers', (t) => {
     t.string('name').notNullable();
     t.string('logo_url', 2000);
     t.string('description', 1000);
-     
+    t.string('email', 254).unique();
+
+    // BELONGS TO addresses
+    t.integer(`address_id`)
+      .unsigned()
+      .references(`addresses.id`)
+      .onDelete('CASCADE');
+
+    addDefaultColumns(t);
   });
 };
 
 exports.down = async (knex) => {
   knex.dropTable('addresses');
+  knex.dropTable('manufacturers');
 };
